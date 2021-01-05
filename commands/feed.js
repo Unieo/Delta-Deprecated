5975191196c402cbb2349e5f20c03e1b111bc2fb
Unieo <unieo@aol.com>
2021-01-05 15:03:13 +0530

    Update feed.js

--- commands/feed.js
+++ commands/feed.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const superagent = require("superagent");
 const config = require("../data/config.json");
-const humor = require("../data/bothumor.json");
 
 module.exports = {
   name: "feed",
