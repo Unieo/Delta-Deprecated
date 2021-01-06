81f743fcddfb07e7b60937fc30caf5d99d52c1c7
Unieo <unieo@aol.com>
2021-01-06 14:30:19 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -15,14 +15,14 @@ module.exports = {
       .setDescription("**Type `d!about` to get more info on the bot**")
       .setTimestamp()
       .setFooter(`Delta is made by ${config.ownername} • ${config.copyright}`);
+    message.reply("Check your DMs!");
     message.author.send({ embed }).catch((e) => {
       if (e) {
+        message.delete();
         message.channel.send(
           `Wait. Your DMs seem to be locked :lock: so I'll send it here instead :wink:`
         );
         message.channel.send({ embed });
-      } else {
-        message.reply("Check your DMs!");
       }
     });
   },
