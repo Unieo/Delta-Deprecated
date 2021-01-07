322cd2234bd6bc92de0907187a96a120952f1d8d
Unieo <unieo@aol.com>
2021-01-07 20:48:45 +0530

    Improved Help Command

--- commands/help.js
+++ commands/help.js
@@ -1,19 +1,19 @@
 const Discord = require("discord.js");
 const config = require("../data/config.json");
-const help = require("../data/help.json");
+const { fun, utility, action, game } = require("../data/help.json");
 
 module.exports = {
   name: "help",
-  cooldown: 5,
+  cooldown: 20,
   execute(client, message, args) {
     const embed = new Discord.MessageEmbed()
       .setTitle("Help is here!")
       .setColor(Math.floor(Math.random() * 16777215))
-      .addField("Fun:", help.fun)
-      .addField("Utility:", help.utility)
-      .addField("Actions:", help.action)
-      .addField("Games:", help.game)
-      .setDescription("**Type `d!about` to get more info on the bot**")
+      .addField("Fun:", fun)
+      .addField("Utility:", utility)
+      .addField("Actions:", action)
+      .addField("Games:", game)
+      .setDescription("**Type `dc!about` to get more info on the bot**")
       .setTimestamp()
       .setFooter(`Delta is made by ${config.ownername} • ${config.copyright}`);
     message.channel.send({ embed });
