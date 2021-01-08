1c41e5f6f6821b2ca48aa4c74dc4a6034ac993be
Unieo <unieo@aol.com>
2021-01-08 13:43:33 +0530

    Bug Fixes, Few Commands Removed
    Removed neko, ngif and kitsune command due to their presence being 'kinda' nsfw for people on discord. Also the Version system is now based on [infinite.15max.15max]

--- commands/about.js
+++ commands/about.js
@@ -12,7 +12,9 @@ module.exports = {
     let licence = "MIT";
     let sourceCode =
       "Click [here](https://github.com/Unieo/Project-Delta) to go to GitHub";
-    let version = "1.1.4 Official Release";
+    let version = "1.1.2 Official Release";
+    let notes =
+      "**Delta Beta:** Like Stable Builds but Awesome Features? Or do you just want to test out new feature's before anyone else? [click here](https://discord.com/oauth2/authorize?client_id=796231105665040414&scope=bot&permissions=2147483647) to invite the Beta version of Delta!\n**Delta Canary:** Are you a dev? Or do you just want to test out new feature's before anyone else? [click here](https://discord.com/oauth2/authorize?client_id=796232722372231219&scope=bot&permissions=2147483647) to invite the Canary version of Delta!";
     const embed = new Discord.MessageEmbed()
       .setTitle("Bot Info")
       .setColor(Math.floor(Math.random() * 16777215))
@@ -21,6 +23,7 @@ module.exports = {
       .addField("Credit:", credit)
       .addField("Licence:", licence)
       .addField("Source Code:", sourceCode)
+      .addField("Other Bots", notes)
       .setTimestamp()
       .setFooter(`Delta is made by ${config.ownername} • ${config.copyright}`);
 
