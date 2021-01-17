96d75b80f279c6755a0ba46fb57d1e7dabf2734e
Neo <unieo@aol.com>
2021-01-17 06:56:25 +0000

    Added bot info on join.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const { prefix } = require("./data/config.json");
+const { prefix, ownername } = require("./data/config.json");
 const keepAlive = require("./server.js");
 const fs = require("fs");
 
@@ -22,6 +22,24 @@ client.on("ready", () => {
   client.user.setActivity(`${prefix}help for help!`, {
     type: "PLAYING",
   });
+});
+
+client.on("guildCreate", guild => {
+	let defaultChannel = "";
+	guild.channels.cache.forEach((channel) => {
+	  if(channel.type == "text" && defaultChannel == "") {
+	    if(channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
+	      defaultChannel = channel;
+	    }
+	  }
+	})
+	const embed = new Discord.MessageEmbed()
+    .setColor(Math.floor(Math.random() * 16777215))
+    .setTitle("Thanks For inviting Delta!")
+		.setDescription(`I hope you enjoy using my commands! Why don't you try \`${prefix}help\` and take a look at my *cool commands*, :smirk:\n\n**Important Links:**\n[Support Server](http://discord.gg/BfwHnRf6bU) - Report Bugs, Request Features, Get Informed of downtime etc.!\n[Invite](https://discord.com/oauth2/authorize?client_id=786882695186874368&scope=bot&permissions=2147483647) - Bot invite link! Invite the bot to your servers...`)
+    .setTimestamp()
+    .setFooter(`Made with ❤️ by ${ownername}`);
+	defaultChannel.send({ embed });
 });
 
 client.on("message", (message) => {
