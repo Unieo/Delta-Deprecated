db232fe5f230462e1d77548bbc1a5f900fa37d5e
Neo <unieo@aol.com>
2021-02-01 11:13:20 +0000

    Help command is much better

--- commands/help.js
+++ commands/help.js
@@ -1,62 +1,65 @@
 const Discord = require("discord.js");
-const pagination = require("discord.js-pagination");
 const config = require("../data/config.json");
-const help = require("../data/help.json");
+const helpMsg = require("../data/help.json");
 
 module.exports = {
 	name: "help",
 	cooldown: 2,
-	execute(client, message, args) {
-		const intro = new Discord.MessageEmbed()
-			.setTitle("Your help has arrived!")
-			.setDescription(
-				"Hello! Thanks for trying out Delta! It helps us out immensely. \n \n React with ⬅️ to move back and ➡️ to move to the next page \n \n **Page 1:** ⁉️ Intro (This Page)  \n **Page 2:** 💥 Reddit Commands \n **Page 3:** 🔘 Utility Commands \n **Page 5:** 🐵 Animal Commands \n **Page 6:** 🎲 Game Commands \n **Page 7:** 🔷 Other Commands \n \n Join our bot's [Support Server](https://discord.gg/BfwHnRf6bU) for bot updates, bug fixes and downtime reminders!"
-			)
-			.setColor(Math.floor(Math.random() * 16777215))
-			.setTimestamp()
-			.setFooter(`Delta is made by ${config.ownername} • ${config.copyright}`);
+	async execute(client, message, args) {
+		let help = args.join(" ").toLowerCase();
 
-		const reddit = new Discord.MessageEmbed()
-			.setTitle("💥 Reddit Commands")
-			.setColor(Math.floor(Math.random() * 16777215))
-			.addField("Reddit:", help.reddit)
-			.setTimestamp()
-			.setFooter(`Delta is made by ${config.ownername} • ${config.copyright}`);
-
-		const utility = new Discord.MessageEmbed()
-			.setTitle("🔘 Utility Commands")
-			.setColor(Math.floor(Math.random() * 16777215))
-			.addField("Utility:", help.utility)
-			.setTimestamp()
-			.setFooter(`Delta is made by ${config.ownername} • ${config.copyright}`);
-
-		const animal = new Discord.MessageEmbed()
-			.setTitle("🐵 Animal Commands")
-			.setColor(Math.floor(Math.random() * 16777215))
-			.addField("Animals:", help.animal)
-			.setTimestamp()
-			.setFooter(`Delta is made by ${config.ownername} • ${config.copyright}`);
-
-		const game = new Discord.MessageEmbed()
-			.setTitle("🎲 Game Commands")
-			.setColor(Math.floor(Math.random() * 16777215))
-			.addField("Games:", help.game)
-			.setTimestamp()
-			.setFooter(`Delta is made by ${config.ownername} • ${config.copyright}`);
-
-		const other = new Discord.MessageEmbed()
-			.setTitle("🔷 Other Commands")
-			.setColor(Math.floor(Math.random() * 16777215))
-			.addField("Other:", help.other)
-			.setTimestamp()
-			.setFooter(`Delta is made by ${config.ownername} • ${config.copyright}`);
-
-		const pages = [intro, reddit, utility, animal, game, other];
-
-		const emojiList = ["⬅️", "➡️"];
-
-		const timeout = "120000";
-
-		pagination(message, pages, emojiList, timeout);
+		if (help === "") {
+			const embed = new Discord.MessageEmbed()
+				.setTitle("List of Delta's Commands")
+				.setColor(Math.floor(Math.random() * 16777215))
+				.addField("💥 Reddit Commands", `\`${config.prefix}help reddit\``, true)
+				.addField("🔘 Utility Commands", `\`${config.prefix}help utility\``, true)
+				.addField("🐵 Animal Commands", `\`${config.prefix}help animal\``, true)
+				.addField("🎲 Game Commands", `\`${config.prefix}help game\``, true)
+				.addField("🔷 Other Commands", `\`${config.prefix}help other\``, true)
+				.setTimestamp()
+				.setFooter(`Delta is made by ${config.ownername} • ${config.copyright}`);
+			message.channel.send({ embed });
+		} else if (help === "reddit") {
+			let reddit = new Discord.MessageEmbed()
+				.setTitle("💥 Reddit Commands")
+				.setColor(Math.floor(Math.random() * 16777215))
+				.addField("Reddit:", helpMsg.reddit)
+				.setTimestamp()
+				.setFooter(`Delta is made by ${config.ownername} • ${config.copyright}`);
+			message.channel.send(reddit)
+		} else if (help === "utility") {
+			let utility = new Discord.MessageEmbed()
+				.setTitle("🔘 Utility Commands")
+				.setColor(Math.floor(Math.random() * 16777215))
+				.addField("Utility:", helpMsg.utility)
+				.setTimestamp()
+				.setFooter(`Delta is made by ${config.ownername} • ${config.copyright}`);
+			message.channel.send(utility)
+		} else if (help === "animal") {
+			let animal = new Discord.MessageEmbed()
+				.setTitle("🐵 Animal Commands")
+				.setColor(Math.floor(Math.random() * 16777215))
+				.addField("Animals:", helpMsg.animal)
+				.setTimestamp()
+				.setFooter(`Delta is made by ${config.ownername} • ${config.copyright}`);
+			message.channel.send(animal)
+		} else if (help === "game") {
+			let game = new Discord.MessageEmbed()
+				.setTitle("🎲 Game Commands")
+				.setColor(Math.floor(Math.random() * 16777215))
+				.addField("Games:", helpMsg.game)
+				.setTimestamp()
+				.setFooter(`Delta is made by ${config.ownername} • ${config.copyright}`);
+			message.channel.send(game)
+		} else if (help === "other") {
+			let other = new Discord.MessageEmbed()
+				.setTitle("🔷 Other Commands")
+				.setColor(Math.floor(Math.random() * 16777215))
+				.addField("Others:", helpMsg.other)
+				.setTimestamp()
+				.setFooter(`Delta is made by ${config.ownername} • ${config.copyright}`);
+			message.channel.send(other)
+		}
 	},
 };
